b85eb8a06567a9b561c4963320b64f338bf1ffc3
Sambeg Shrestha <sambeg.shrestha@konnectcraft.com>
2025-05-06 15:18:13 +0545

    feat: export api-client

--- packages/api-client/index.ts
+++ packages/api-client/index.ts
@@ -1,8 +1,5 @@
 import { hc } from 'hono/client'
 
-import { AppType } from '@apps/api'
+import type { AppType } from '@apps/api'
 
-const client = hc<AppType>('')
-export type Client = typeof client
-
-export default (...args: Parameters<typeof hc>): Client => hc<AppType>(...args)
+export default (...args: Parameters<typeof hc>) => hc<AppType>(...args)
